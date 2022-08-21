a41949572e70bfb37c4cdbabfa2e01ed204e90a2
Dhruv Maniya <81362979+DhruvManiya@users.noreply.github.com>
2022-08-21 19:11:03 +0530

    magazines page done

--- src/pages/theMagazines.jsx
+++ src/pages/theMagazines.jsx
@@ -3,108 +3,164 @@ import classes from '../styles/sass/magazines.module.scss'
 import { Carousel } from '@trendyol-js/react-carousel';
 import {useState , useEffect} from 'react'
 
+var swaping = false;
+var itemShow =9.5;
 
 function theComic() {
-    const [magazines,setMagazines] = useState(null);
-    const [searchValue,setSearchValue] = useState('');
-    const [items,setItems] = useState([]);
+    const [businesses,setBusinesses] = useState(null);
+    const [foods,setFoods] = useState(null);
+    const [fitnesses,setFitnesses] = useState(null);
+    const [science,setScience] = useState(null);
+    const [health,setHealth] = useState(null);
     
-    useEffect(()=>{
-        
-        getUser(searchValue.trim().replaceAll(" ",'+'));
-        setItems([]);
-
-    },[searchValue]);
-    
- 
-    
-
-    async function getUser(userSerch) {
+  async function fetchMagazine() {
         try {
-          const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=+${userSerch}+intitle&printType=magazines&projection=full&key=AIzaSyAr3fNjJy4OTQfHW2CYdjOvPs04aPWvezQ`);
-          console.log(userSerch);
-          if(!res.data.hasOwnProperty('items') && userSerch === ''){
-            setItems([]);
-          }else{
-            setItems(res.data.items);
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-
-    async function fetchMagazine() {
-        try {
-            const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=Magazine&printType=magazines&orderBy=newest&key=AIzaSyAr3fNjJy4OTQfHW2CYdjOvPs04aPWvezQ`);
-            if(!res.data.hasOwnProperty('items')){
-                setMagazines(null);
+            const res1 = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=Business+subject&maxResults=20&printType=magazines&orderBy=newest&key=AIzaSyAr3fNjJy4OTQfHW2CYdjOvPs04aPWvezQ`);
+            const res2 = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=Food+subject&maxResults=20&printType=magazines&orderBy=newest&key=AIzaSyAr3fNjJy4OTQfHW2CYdjOvPs04aPWvezQ`);
+            const res3 = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=Fitness+subject&maxResults=20&printType=magazines&orderBy=newest&key=AIzaSyAr3fNjJy4OTQfHW2CYdjOvPs04aPWvezQ`);
+            const res4 = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=Science+subject&maxResults=20&printType=magazines&orderBy=newest&key=AIzaSyAr3fNjJy4OTQfHW2CYdjOvPs04aPWvezQ`);
+            const res5 = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=Health+subject&maxResults=20&printType=magazines&orderBy=newest&key=AIzaSyAr3fNjJy4OTQfHW2CYdjOvPs04aPWvezQ`);
+            if(!res1.data.hasOwnProperty('items')){
+                setBusinesses(null);
             }else{
-                setMagazines(res.data.items);
+                setBusinesses(res1.data.items);
+            }
+            if(!res2.data.hasOwnProperty('items')){
+                setFoods(null);
+            }else{
+                setFoods(res2.data.items);
+            }
+            if(!res3.data.hasOwnProperty('items')){
+                setFitnesses(null);
+            }else{
+                setFitnesses(res3.data.items);
+            }
+            if(!res4.data.hasOwnProperty('items')){
+                setScience(null);
+            }else{
+                setScience(res4.data.items);
+            }
+            if(!res5.data.hasOwnProperty('items')){
+                setHealth(null);
+            }else{
+                setHealth(res5.data.items);
             }
         } catch (error) {
             console.error(error);
         }
     };
-    console.log(items);
+    
     
     useEffect(() => {
         fetchMagazine();
       }, []);
     return (
         <>
-            
-        <div className={classes.mainSection}>
-            <h1 className={classes.heading}>Search your magazine</h1>
-            <input type="search" placeholder="Search magazines by it's name" onChange={e => setSearchValue(e.target.value)}/>
-            <div className={classes.div}>
-                <ul className={classes.ul}>
-                
-                {
-                !items ? 
-                    <p>Items Not Found</p>
-                : 
-                    items.map((item) => {
-                        return <li key={item.id}>
-                            {!item.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt={searchValue}/> :<img src={item.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
-                            <div className={classes.bookAutherTitle}>
-                            {!item.volumeInfo.hasOwnProperty('title') ? <p></p> : <p>{item.volumeInfo.title}</p>}
-                            {!item.volumeInfo.hasOwnProperty('authors') ? <i></i> :<i>{item.volumeInfo.authors[0]}</i>}
-                            </div>
-                        </li>
-                    })
-                
-                }
-                </ul>
-            </div>
-            
-        </div>
 
 
-        <h1 className={classes.title}>Top 10 Magazines</h1>
+        <h1 className={classes.title}>Business Magazines</h1>
         
-        
-        
-        
-        
-        {!magazines ?
+        {!businesses ?
         <p>books not found</p>
         :
-        <Carousel show={9.5} slide={1} swiping={false} >
+        <Carousel show={itemShow} slide={1} swiping={swaping} >
 		
 
-        {magazines.map((magazine) => {
-            return <li className={classes.li} key={magazine.id}>
-                {!magazine.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt='magazine not found'/> :<img src={magazine.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
+        {businesses.map((business) => {
+            return <li className={classes.li} key={business.id}>
+                {!business.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt='magazine not found'/> :<img src={business.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
                 <section className={classes.briffDesc}>
-                    {!magazine.volumeInfo.hasOwnProperty('title') ? <h1></h1> : <h1>{magazine.volumeInfo.title}</h1>}
-                    {!magazine.volumeInfo.hasOwnProperty('publishedDate') ? <p></p> : <p>published in : {magazine.volumeInfo.publishedDate}</p>}
-                    {!magazine.volumeInfo.hasOwnProperty('language') ? <i></i> :<p><i>language - </i><b>{magazine.volumeInfo.language}</b></p>}
+                    {!business.volumeInfo.hasOwnProperty('title') ? <h1></h1> : <h1>{business.volumeInfo.title}</h1>}
+                    {!business.volumeInfo.hasOwnProperty('publishedDate') ? <p></p> : <p>published in : {business.volumeInfo.publishedDate}</p>}
+                    {!business.volumeInfo.hasOwnProperty('language') ? <i></i> :<p><i>language - </i><b>{business.volumeInfo.language}</b></p>}
+                </section>
+            </li>
+        })}
+
+        </Carousel>}
+        <h1 className={classes.title}>Food Magazines</h1>
+        
+        {!foods ?
+        <p>Magazines not found</p>
+        :
+        <Carousel show={itemShow} slide={1} swiping={swaping} >
+		
+
+        {foods.map((food) => {
+            return <li className={classes.li} key={food.id}>
+                {!food.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt='magazine not found'/> :<img src={food.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
+                <section className={classes.briffDesc}>
+                    {!food.volumeInfo.hasOwnProperty('title') ? <h1></h1> : <h1>{food.volumeInfo.title}</h1>}
+                    {!food.volumeInfo.hasOwnProperty('publishedDate') ? <p></p> : <p>published in : {food.volumeInfo.publishedDate}</p>}
+                    {!food.volumeInfo.hasOwnProperty('language') ? <i></i> :<p><i>language - </i><b>{food.volumeInfo.language}</b></p>}
                 </section>
             </li>
         })}
 
         </Carousel>}
 
+        <h1 className={classes.title}>Health Magazines</h1>
+        
+        {!health ?
+        <p>Magazines not found</p>
+        :
+        <Carousel show={itemShow} slide={1} swiping={swaping} >
+		
+
+        {health.map((item) => {
+            return <li className={classes.li} key={item.id}>
+                {!item.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt='magazine not found'/> :<img src={item.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
+                <section className={classes.briffDesc}>
+                    {!item.volumeInfo.hasOwnProperty('title') ? <h1></h1> : <h1>{item.volumeInfo.title}</h1>}
+                    {!item.volumeInfo.hasOwnProperty('publishedDate') ? <p></p> : <p>published in : {item.volumeInfo.publishedDate}</p>}
+                    {!item.volumeInfo.hasOwnProperty('language') ? <i></i> :<p><i>language - </i><b>{item.volumeInfo.language}</b></p>}
+                </section>
+            </li>
+        })}
+
+        </Carousel>}
+
+        <h1 className={classes.title}>Fitnesses Magazines</h1>
+        
+        {!fitnesses ?
+        <p>Magazines not found</p>
+        :
+        <Carousel show={itemShow} slide={1} swiping={swaping} >
+		
+
+        {fitnesses.map((fitness) => {
+            return <li className={classes.li} key={fitness.id}>
+                {!fitness.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt='magazine not found'/> :<img src={fitness.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
+                <section className={classes.briffDesc}>
+                    {!fitness.volumeInfo.hasOwnProperty('title') ? <h1></h1> : <h1>{fitness.volumeInfo.title}</h1>}
+                    {!fitness.volumeInfo.hasOwnProperty('publishedDate') ? <p></p> : <p>published in : {fitness.volumeInfo.publishedDate}</p>}
+                    {!fitness.volumeInfo.hasOwnProperty('language') ? <i></i> :<p><i>language - </i><b>{fitness.volumeInfo.language}</b></p>}
+                </section>
+            </li>
+        })}
+
+        </Carousel>}
+        <h1 className={classes.title}>Science Magazines</h1>
+        
+        {!science ?
+        <p>Magazines not found</p>
+        :
+        <Carousel show={itemShow} slide={1} swiping={swaping} >
+		
+
+        {science.map((item) => {
+            return <li className={classes.li} key={item.id}>
+                {!item.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt='magazine not found'/> :<img src={item.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
+                <section className={classes.briffDesc}>
+                    {!item.volumeInfo.hasOwnProperty('title') ? <h1></h1> : <h1>{item.volumeInfo.title}</h1>}
+                    {!item.volumeInfo.hasOwnProperty('publishedDate') ? <p></p> : <p>published in : {item.volumeInfo.publishedDate}</p>}
+                    {!item.volumeInfo.hasOwnProperty('language') ? <i></i> :<p><i>language - </i><b>{item.volumeInfo.language}</b></p>}
+                </section>
+            </li>
+        })}
+
+        </Carousel>}
+        
 
         </>
     )
