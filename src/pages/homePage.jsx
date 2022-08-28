b9d4016b8d2f1e1c7de972f407d8d8b5fb0d8f88
Dhruv Maniya <81362979+DhruvManiya@users.noreply.github.com>
2022-08-28 17:18:48 +0530

    homePage,footer,nevbar responsive done

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -4,17 +4,23 @@ import {useState , useEffect, useId} from 'react'
 import {Link} from 'react-router-dom'
 
 
+
+
   
 function homePage() {
     const [searchValue,setSearchValue] = useState('');
     const [items,setItems] = useState([]);
+    const [ul,setUl] = useState(true);
 
 
-    
-    
     useEffect(()=>{
         
         getUser(searchValue.replaceAll(" ",'+'));
+        if(searchValue===''){
+          setUl(true)
+        }else{
+          setUl(false)
+        }
 
     },[searchValue]);
     
@@ -40,32 +46,39 @@ function homePage() {
     return (
         <div className={classes.mainSection}>
             <h1 className={classes.heading}>Search your next book</h1>
-            <input type="search" placeholder="Search books by it's name" onChange={e => setSearchValue(e.target.value)}/>
-            <div className={classes.div}>
-                <ul className={classes.ul}>
-                
-                {
-                !items.length ? 
-                    <p>Items Not Found</p>
-                : 
-                    items.map((item) => {
-                        return <>{item.volumeInfo.hasOwnProperty('industryIdentifiers') ? 
-                        <Link to={`/books/${item.volumeInfo.industryIdentifiers[0].identifier}`}>
-                            <li key={item.id}>
-                              {!item.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt={searchValue}/> :<img src={item.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
-                              <div className={classes.bookAutherTitle}>
-                              {!item.volumeInfo.hasOwnProperty('title') ? <p></p> : <p>{item.volumeInfo.title}</p>}
-                              {!item.volumeInfo.hasOwnProperty('authors') ? <i></i> :<i>{item.volumeInfo.authors[0]}</i>}
-                              </div>
-                          </li>
-                        </Link> :
-                        <></>}</>
-                    })
-                
-                }
-                </ul>
-            </div>
             <p>We gives rating of books so your next book become better for you</p>
+
+
+              <input type="search" placeholder="Search books by it's name" onChange={e => setSearchValue(e.target.value)}/>
+              <div className={classes.div}>
+                  {ul ? 
+                  <></>  : 
+                  <ul className={classes.ul}>
+                  
+                  {
+                  !items.length ? 
+                      <p>Items Not Found</p>
+                  : 
+                      items.map((item) => {
+                          return <>{item.volumeInfo.hasOwnProperty('industryIdentifiers') ? 
+                          <Link to={`/books/${item.volumeInfo.industryIdentifiers[0].identifier}`}>
+                              <li key={item.id}>
+                                {!item.volumeInfo.hasOwnProperty('imageLinks') ? <img src='' alt={searchValue}/> :<img src={item.volumeInfo.imageLinks.smallThumbnail} loading="lazy"/>}
+                                <div className={classes.bookAutherTitle}>
+                                {!item.volumeInfo.hasOwnProperty('title') ? <p></p> : <p className={classes.resDesc}>{item.volumeInfo.title}</p>}
+                                {!item.volumeInfo.hasOwnProperty('authors') ? <i></i> :<i className={classes.resDesc}>{item.volumeInfo.authors[0]}</i>}
+                                </div>
+                            </li>
+                          </Link> :
+                          <></>}</>
+                      })
+                  
+                  }
+                  </ul>
+                  }
+                  
+
+            </div>
         </div>
 
 
--- src/ui/theNevbar.jsx
+++ src/ui/theNevbar.jsx
@@ -1,18 +1,29 @@
 import classes from '../styles/sass/nevbar.module.scss'
 
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function theNevbar() {
+
+
     return(
-        <header className={classes.header}>
-            <Link to='/'><img src="../../images/book_shelf-removebg-preview.png" /></Link>
-            <div className={classes.div}>
-                <Link to='/home'><h3>Home</h3></Link>
-                <Link to='/books'><h3>Books</h3></Link>
-                <Link to='/magazines'><h3>Magazines</h3></Link>
-            
-            </div>
-        </header>
+        <>
+            <header className={`${classes.header}`}>
+                <Link to='/'><img src="../../images/book_shelf-removebg-preview.png" /></Link>
+                <div className={`${classes.responsiveDisplayNone} ${classes.div}`}>
+                    <Link to='/home'><h3 className={`${classes.displayBtn}`}>Home</h3></Link>
+                    <Link to='/books'><h3  className={`${classes.displayBtn}`}>Books</h3></Link>
+                    <Link to='/magazines'><h3  className={`${classes.displayBtn}`}>Magazines</h3></Link>            
+                </div>
+            </header>
+            <footer className={`${classes.displayNone} ${classes.header}`}>
+                <div className={`${classes.div}`}>
+                    <Link to='/home'><h3 className={`${classes.displayBtn}`}>Home</h3></Link>
+                    <Link to='/books'><h3 className={`${classes.displayBtn}`}>Books</h3></Link>
+                    <Link to='/magazines'><h3 className={`${classes.displayBtn}`}>Magazines</h3></Link>            
+                </div>
+            </footer>
+        </>
     )
 }
 
