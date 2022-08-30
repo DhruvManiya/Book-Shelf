3791b12e2b906782616e1e16918d3160e5672342
Dhruv Maniya <81362979+DhruvManiya@users.noreply.github.com>
2022-08-30 21:03:44 +0530

    test div

--- src/ui/theNevbar.jsx
+++ src/ui/theNevbar.jsx
@@ -8,7 +8,7 @@ function theNevbar() {
     return(
         <>
             <header className={`${classes.header}`}>
-                <img src="../../images/bookshelf.jpg" alt="logo" />
+                <Link to='/'><div></div></Link>
                 <div className={`${classes.responsiveDisplayNone} ${classes.div}`}>
                     <Link to='/home'><h3 className={`${classes.displayBtn}`}>Home</h3></Link>
                     <Link to='/books'><h3  className={`${classes.displayBtn}`}>Books</h3></Link>
